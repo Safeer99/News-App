a6ceda8dce3a16f741b21d6e938cda9b2a2fe0d1
Safeer Anwar <98336331+Safeer99@users.noreply.github.com>
2022-03-19 18:44:43 +0530

    newsapp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import Navbar from './components/Navbar';
+import News from './components/News';
+import LoadingBar from 'react-top-loading-bar';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+    pageSize = 12;
+    country = 'in';
+    apiKey = process.env.REACT_APP_NEWS_API
+
+    state = {
+        progress:0
+    }
+    setProgress = (progress) => {
+        this.setState({progress:progress})
+    }
+
+    render() {
+        return (
+            <div>
+                <Router>
+                    <Navbar/>
+                    <LoadingBar
+                        height={3}
+                        color='#f11946'
+                        progress={this.state.progress}
+                    />
+                    <Switch>
+                        <Route exact path="/">
+                            <News setProgress={this.setProgress} key="general" country={this.country} apiKey={this.apiKey} category="general" pageSize={this.pageSize} />
+                        </Route>
+                        <Route exact path="/sports">
+                            <News setProgress={this.setProgress} key="sports" country={this.country}  apiKey={this.apiKey} category="sports" pageSize={this.pageSize} />
+                        </Route>
+                        <Route exact path="/entertainment">
+                            <News setProgress={this.setProgress} key="entertainment" country={this.country}  apiKey={this.apiKey} category="entertainment" pageSize={this.pageSize} />
+                        </Route>
+                        <Route exact path="/business">
+                            <News setProgress={this.setProgress} key="business" country={this.country}  apiKey={this.apiKey} category="business" pageSize={this.pageSize} />
+                        </Route>
+                        <Route exact path="/science">
+                            <News setProgress={this.setProgress} key="science" country={this.country}  apiKey={this.apiKey} category="science" pageSize={this.pageSize} />
+                        </Route>
+                        <Route exact path="/health">
+                            <News setProgress={this.setProgress} key="health" country={this.country}  apiKey={this.apiKey} category="health" pageSize={this.pageSize} />
+                        </Route>
+                        <Route exact path="/technology">
+                            <News setProgress={this.setProgress} key="technology" country={this.country}  apiKey={this.apiKey} category="technology" pageSize={this.pageSize} />
+                        </Route>
+                    </Switch>
+                </Router>
+            </div>
+        )
+    }
 }
-
-export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import {Link} from "react-router-dom";
+
+export class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+                    <div className="container-fluid">
+                        <Link className="navbar-brand" to="/">
+                            <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 16 16" height="1.5em" width="1.5em" xmlns="http://www.w3.org/2000/svg">
+                                <path fillRule="evenodd" d="M0 2A1.5 1.5 0 011.5.5h11A1.5 1.5 0 0114 2v12a1.5 1.5 0 01-1.5 1.5h-11A1.5 1.5 0 010 14V2zm1.5-.5A.5.5 0 001 2v12a.5.5 0 00.5.5h11a.5.5 0 00.5-.5V2a.5.5 0 00-.5-.5h-11z" clipRule="evenodd"></path>
+                                <path fillRule="evenodd" d="M15.5 3a.5.5 0 01.5.5V14a1.5 1.5 0 01-1.5 1.5h-3v-1h3a.5.5 0 00.5-.5V3.5a.5.5 0 01.5-.5z" clipRule="evenodd"></path>
+                                <path d="M2 3h10v2H2V3zm0 3h4v3H2V6zm0 4h4v1H2v-1zm0 2h4v1H2v-1zm5-6h2v1H7V6zm3 0h2v1h-2V6zM7 8h2v1H7V8zm3 0h2v1h-2V8zm-3 2h2v1H7v-1zm3 0h2v1h-2v-1zm-3 2h2v1H7v-1zm3 0h2v1h-2v-1z"></path>
+                            </svg> News</Link>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li className="nav-item">
+                                    <Link className="nav-link" aria-current="page" to="/">Home</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" aria-current="page" to="/sports">Sports</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" aria-current="page" to="/entertainment">Entertainment</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" aria-current="page" to="/business">Business</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" aria-current="page" to="/health">Health</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" aria-current="page" to="/science">Science</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" aria-current="page" to="/technology">Technology</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        )
+    }
+}
+
+export default Navbar
